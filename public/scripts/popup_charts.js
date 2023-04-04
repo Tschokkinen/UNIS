e2f49f4bbe263902167c3456de823a7d8723feba
iirasalo <iira.salo@metropolia.fi>
2023-04-04 17:50:06 +0300

    Textareas, labels, responsivity. Modals are now scrollable and backdrop static.

--- public/scripts/popup_charts.js
+++ public/scripts/popup_charts.js
@@ -3,13 +3,13 @@ function openHRVpopup(url) {
 }  
 
 function openBPpopup(url) {
-    newwindow=window.open(url,'name','height=600,width=1000');if (window.focus) {newwindow.focus()}return false;
+    newwindow=window.open(url,'name','height=550,width=1000');if (window.focus) {newwindow.focus()}return false;
 }
 
 function openSleepMoodpopup(url) {
-    newwindow=window.open(url,'name','height=600,width=1000');if (window.focus) {newwindow.focus()}return false;
+    newwindow=window.open(url,'name','height=500,width=1000');if (window.focus) {newwindow.focus()}return false;
 }
 
 function openCommentspopup(url) {
-    newwindow=window.open(url,'name','height=600,width=1000');if (window.focus) {newwindow.focus()}return false;
+    newwindow=window.open(url,'name','height=550,width=1000');if (window.focus) {newwindow.focus()}return false;
 }
