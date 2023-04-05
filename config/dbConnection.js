e9c681daf56bb5a96871431d92456f5851e9c42e
Tschokkinen <tschokkinen@gmail.com>
2023-04-05 21:43:20 +0300

    Comment model added plus related end points. Also, table generated for the comments.

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Role = require('../models/RoleModel');
 
 const connectDB = async () => {
     try {
@@ -6,9 +7,28 @@ const connectDB = async () => {
             useUnifiedTopology: true,
             useNewUrlParser: true
         });
+        await getRoles();
     } catch (err) {
         console.log(err);
     }
 }
 
 module.exports = connectDB;
+
+
+
+// initUserRoles();
+
+const getRoles = async () => {
+    let count = await Role.estimatedDocumentCount();
+    console.log(count);
+    if (count === 0) {
+        console.log("Zero roles");
+        await Role.create({
+            name: "user"
+        });
+        console.log("Created role");
+    }
+    console.log("Roles already exist.");
+}
+
