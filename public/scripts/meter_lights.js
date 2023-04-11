48f53e9e29f4bf89c7f332d4524b32211931437c
Tschokkinen <tschokkinen@gmail.com>
2023-04-11 22:07:32 +0300

    Fixed errors resulting from manual merge.

--- public/scripts/meter_lights.js
+++ public/scripts/meter_lights.js
@@ -8,21 +8,33 @@ const setMeterLights = async () => {
     console.log("Data: ", data);
 
     if (data.sleep === true) {
+        sleepMeterLight.title = "You have saved results for today."
         sleepMeterLight.src = "assets/img/green_light.png"
+        sleepMeterLight.alt = "Sleep meter. Green light. Measured today."
     } else {
+        sleepMeterLight.title = "No saved results for today."
         sleepMeterLight.src = "assets/img/red_light.png"
+        sleepMeterLight.alt = "Sleep meter. Red light. Not measured today."
     }
 
     if (data.mood === true) {
+        moodMeterLight.title = "You have saved results for today."
         moodMeterLight.src = "assets/img/green_light.png"
+        moodMeterLight.alt = "Mood meter. Green light. Measured today."
     } else {
+        moodMeterLight.title = "No saved results for today."
         moodMeterLight.src = "assets/img/red_light.png"
+        moodMeterLight.alt = "Mood meter. Red light. Not measured today."
     }
 
     if (data.bloodpressure === true) {
+        bloodpressureMeterLight.title = "You have saved results for today."
         bloodpressureMeterLight.src = "assets/img/green_light.png"
+        bloodpressureMeterLight.alt = "Bloodpressure. Green light. Measured today."
     } else {
+        bloodpressureMeterLight.title = "No saved results for today."
         bloodpressureMeterLight.src = "assets/img/red_light.png"
+        bloodpressureMeterLight.alt = "Bloodpressure. Red light. Not measured today."
     }
 };
 
