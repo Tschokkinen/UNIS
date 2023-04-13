de85f692f5a53d169ca29f127cb15fdad91b9294
iirasalo <iira.salo@metropolia.fi>
2023-04-14 00:32:49 +0300

    Webforms send name and email on submit

--- public/scripts/requestUserData.js
+++ public/scripts/requestUserData.js
@@ -10,6 +10,8 @@ requestUserData();
 
 const setDefaultValue = (userData) => {
     // const height = userData.height != undefined || null ? userData.height : ""; 
+
+    // At change user info and main view: "";
     document.getElementById('field_FirstName').defaultValue = userData.firstName != undefined || null ? userData.firstName : "";
     document.getElementById('field_LastName').defaultValue = userData.lastName != undefined || null ? userData.lastName : "";
     document.getElementById('field_Height').defaultValue = userData.height != undefined || null ? userData.height : "";
@@ -17,4 +19,14 @@ const setDefaultValue = (userData) => {
     document.getElementById('field_Email').defaultValue = userData.email != undefined || null ? userData.email : "";
     document.getElementById('field_Age').defaultValue = userData.age != undefined || null ? userData.age : "";
     document.getElementById('field_Phonenumber').defaultValue = userData.phonenumber != undefined || null ? userData.phonenumber : "";
+    
+    // At elements on send message to support/professional : "";
+    document.getElementById('field_FirstName_msgSupport').defaultValue = userData.firstName != undefined || null ? userData.firstName : "";
+    document.getElementById('field_LastName_msgSupport').defaultValue = userData.lastName != undefined || null ? userData.lastName : "";
+    document.getElementById('field_Email_msgSupport').defaultValue = userData.email != undefined || null ? userData.email : "";
+    
+    document.getElementById('field_FirstName_msgPro').defaultValue = userData.firstName != undefined || null ? userData.firstName : "";
+    document.getElementById('field_LastName_msgPro').defaultValue = userData.lastName != undefined || null ? userData.lastName : "";
+    document.getElementById('field_Email_msgPro').defaultValue = userData.email != undefined || null ? userData.email : "";
+    
 }
