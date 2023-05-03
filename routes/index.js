eac8df386a01cc933792352d1c4b01dec0ed7d9d
Tschokkinen <tschokkinen@gmail.com>
2023-05-03 22:31:03 +0300

    Fixed issue where user could access pro features in chart view.

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
 const router = require('express').Router();
 const csrf = require('csurf');
 const csrfProtection = csrf({ cookie: true });
+const { clearChartCookie } = require('../middleware/clearCookies');
 
 const { signInPage, signInPagePro, signIn, registerPage, registerUser } = require('../controllers/indexController');
 
 router.route('/')
     .get(csrfProtection, signInPage)
-    .post(csrfProtection, signIn);
+    .post(csrfProtection, clearChartCookie, signIn);
 
 router.route('/pro')
     .get(csrfProtection, signInPagePro)
-    .post(csrfProtection, signIn);
+    .post(csrfProtection, clearChartCookie, signIn);
 
 router.route('/register')
     .get(registerPage)
